720b95e638db6b9a1600b633380a72b0e35bb455
obelisk477 <cwbatte2@gmail.com>
2023-06-27 20:04:12 -0400

    Configured sequelize config for jawsdb; added session store to server

--- server.js
+++ server.js
@@ -3,22 +3,31 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const routes = require('./controllers')
 const session = require('express-session')
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express()
 const PORT = process.env.PORT || 3001
+const sequelize = require('./config/connection')
+
 
 const sessionConfig = {
     secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
     cookie: {
-        maxAge: 60*60*1000
-    }
-}
+      maxAge: 300000,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
 
 app.use(session(sessionConfig))
 
-const sequelize = require('./config/connection')
+
 const hbs = exphbs.create({})
 
 app.engine('handlebars', hbs.engine)
